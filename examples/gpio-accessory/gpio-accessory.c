e011a0539f5be200c166a270fcaa41fedb9a98e4
JohnWickham <john@wjwickham.com>
2020-06-01 18:10:43 -0400

    Use wifi_config

--- examples/gpio-accessory/gpio-accessory.c
+++ examples/gpio-accessory/gpio-accessory.c
@@ -8,86 +8,75 @@
 
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
-#include "wifi.h"
+#include <wifi_config.h>
 
+const int accessory_gpio = 12;
+const int onboard_led_gpio = 2;
+bool accessory_on = false;
 
-static void wifi_init() {
-    struct sdk_station_config wifi_config = {
-        .ssid = WIFI_SSID,
-        .password = WIFI_PASSWORD,
-    };
-
-    sdk_wifi_set_opmode(STATION_MODE);
-    sdk_wifi_station_set_config(&wifi_config);
-    sdk_wifi_station_connect();
+void write(int pin, bool on) {
+    gpio_write(pin, on ? 0 : 1);
 }
 
-const int led_gpio = 2;
-bool led_on = false;
-
-void led_write(bool on) {
-    gpio_write(led_gpio, on ? 0 : 1);
-}
-
-void led_init() {
-    gpio_enable(led_gpio, GPIO_OUTPUT);
-    led_write(led_on);
+void accessory_init() {
+    gpio_enable(onboard_led_gpio, GPIO_OUTPUT);
+    gpio_enable(accessory_gpio, GPIO_OUTPUT);
+    write(accessory_on);
 }
 
 void led_identify_task(void *_args) {
     for (int i=0; i<3; i++) {
         for (int j=0; j<2; j++) {
-            led_write(true);
+            write(onboard_led_gpio, true);
             vTaskDelay(100 / portTICK_PERIOD_MS);
-            led_write(false);
+            write(onboard_led_gpio, false);
             vTaskDelay(100 / portTICK_PERIOD_MS);
         }
 
         vTaskDelay(250 / portTICK_PERIOD_MS);
     }
 
-    led_write(led_on);
+    write(onboard_led_gpio, accessory_on);
 
     vTaskDelete(NULL);
 }
 
 void led_identify(homekit_value_t _value) {
-    printf("LED identify\n");
     xTaskCreate(led_identify_task, "LED identify", 128, NULL, 2, NULL);
 }
 
-homekit_value_t led_on_get() {
-    return HOMEKIT_BOOL(led_on);
+homekit_value_t accessory_on_get() {
+    return HOMEKIT_BOOL(accessory_on);
 }
 
-void led_on_set(homekit_value_t value) {
+void accessory_on_set(homekit_value_t value) {
     if (value.format != homekit_format_bool) {
         printf("Invalid value format: %d\n", value.format);
         return;
     }
 
-    led_on = value.bool_value;
-    led_write(led_on);
+    accessory_on = value.bool_value;
+    write(accessory_gpio, accessory_on);
 }
 
 
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
-            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HaPK"),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "037A2BABF19D"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "MyLED"),
-            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+            HOMEKIT_CHARACTERISTIC(NAME, "Side Table Accent Light"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "John Wickham"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "20200601"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "Triumph 1–P"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
             HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify),
             NULL
         }),
         HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
+            HOMEKIT_CHARACTERISTIC(NAME, "Accent Light"),
             HOMEKIT_CHARACTERISTIC(
                 ON, false,
-                .getter=led_on_get,
-                .setter=led_on_set
+                .getter=accessory_on_get,
+                .setter=accessory_on_set
             ),
             NULL
         }),
@@ -98,13 +87,16 @@ homekit_accessory_t *accessories[] = {
 
 homekit_server_config_t config = {
     .accessories = accessories,
-    .password = "111-11-111"
+    .password = "481-51-623"
 };
+
+void on_wifi_ready() {
+    homekit_server_init(&config);
+}
 
 void user_init(void) {
     uart_set_baud(0, 115200);
 
-    wifi_init();
-    led_init();
-    homekit_server_init(&config);
+    wifi_config_init("my-accessory", NULL, on_wifi_ready);
+    accessory_init();
 }
