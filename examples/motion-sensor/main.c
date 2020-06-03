d184f51c3264eabac6af5a760895d85969371e36
JohnWickham <john@wjwickham.com>
2020-06-03 11:08:52 -0400

    Contributions from AchimPieters

--- examples/motion-sensor/main.c
+++ examples/motion-sensor/main.c
@@ -1,102 +1,115 @@
-/*
- * Motion sensor for HomeKit.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <espressif/esp_wifi.h>
-#include <espressif/esp_sta.h>
+#include <espressif/esp_common.h>
 #include <esp/uart.h>
 #include <esp8266.h>
 #include <FreeRTOS.h>
 #include <task.h>
-#include <math.h>
+#include <queue.h>
+#include <string.h>
 
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
-
-#include <toggle.h>
 #include <wifi_config.h>
 
-const int onboard_led_gpio = 2;
-const int sensor_pin = 16;
+const int led_gpio = 2;
 
-void led_init() {
-    gpio_enable(onboard_led_gpio, GPIO_OUTPUT);
+#define MOTION_SENSOR_GPIO 16
+
+void led_write(bool on) {
+    gpio_write(led_gpio, on ? 0 : 1);
 }
 
-void identify_accessory() {
+void identify_task(void *_args) {
+    // We identify the `Motion Sensor` by Flashing it's LED.
     for (int i=0; i<3; i++) {
         for (int j=0; j<2; j++) {
-            gpio_write(onboard_led_gpio, 1);
+            led_write(true);
             vTaskDelay(100 / portTICK_PERIOD_MS);
-            gpio_write(onboard_led_gpio, 0);
+            led_write(false);
             vTaskDelay(100 / portTICK_PERIOD_MS);
         }
 
         vTaskDelay(250 / portTICK_PERIOD_MS);
     }
 
-    gpio_write(onboard_led_gpio, 0);
+    led_write(false);
 
     vTaskDelete(NULL);
 }
 
-homekit_characteristic_t motion_detected = HOMEKIT_CHARACTERISTIC_(MOTION_DETECTED, 0);
-
-void sensor_callback(bool state, void *context) {
-    motion_detected.value = HOMEKIT_UINT8(state ? 1 : 0);
-    homekit_characteristic_notify(&motion_detected, motion_detected.value);
-    gpio_write(onboard_led_gpio, state ? 1 : 0);
+void identify(homekit_value_t _value) {
+    xTaskCreate(identify_task, "Identify", 128, NULL, 2, NULL);
 }
 
+homekit_characteristic_t motion_detected  = HOMEKIT_CHARACTERISTIC_(MOTION_DETECTED, 0);
+
+void motion_sensor_callback(uint8_t gpio) {
+
+    if (gpio == MOTION_SENSOR_GPIO){
+        int new = 0;
+        new = gpio_read(MOTION_SENSOR_GPIO);
+        motion_detected.value = HOMEKIT_BOOL(new);
+        homekit_characteristic_notify(&motion_detected, HOMEKIT_BOOL(new));
+    }
+
+}
+
+void gpio_init() {
+
+    gpio_enable(MOTION_SENSOR_GPIO, GPIO_INPUT);
+    gpio_set_pullup(MOTION_SENSOR_GPIO, false, false);
+    gpio_set_interrupt(MOTION_SENSOR_GPIO, GPIO_INTTYPE_EDGE_ANY, motion_sensor_callback);
+}
+
+homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "Motion Sensor");
+
 homekit_accessory_t *accessories[] = {
-    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]) {
-        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
-            HOMEKIT_CHARACTERISTIC(NAME, "Motion Sensor"),
-            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "John Wickham"),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "20200601"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "Motion Sensor"),
-            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
-            HOMEKIT_CHARACTERISTIC(IDENTIFY, identify_accessory),
-            NULL
-        }),
-        HOMEKIT_SERVICE(MOTION_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
-            HOMEKIT_CHARACTERISTIC(NAME, "Motion Sensor"),
-            &motion_detected,
-            NULL
+        HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]){
+                HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+                        &name,
+                        HOMEKIT_CHARACTERISTIC(MANUFACTURER, "John Wickham"),
+                        HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "20200603"),
+                        HOMEKIT_CHARACTERISTIC(MODEL, "Motion Sensor"),
+                        HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+                        HOMEKIT_CHARACTERISTIC(IDENTIFY, identify),
+                        NULL
+                }),
+                HOMEKIT_SERVICE(MOTION_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+                        HOMEKIT_CHARACTERISTIC(NAME, "Motion Sensor"),
+                        &motion_detected,
+                        NULL
+                }),
+                NULL
         }),
         NULL
-    }),
-    NULL
 };
 
-
-static bool homekit_initialized = false;
-static homekit_server_config_t config = {
+homekit_server_config_t config = {
     .accessories = accessories,
     .password = "481-51-623",
     .setupId="1QJ8"
 };
 
-
-void on_wifi_config_event(wifi_config_event_t event) {
-    if (event == WIFI_CONFIG_CONNECTED) {
-        if (!homekit_initialized) {
-            homekit_server_init(&config);
-            homekit_initialized = true;
-        }
-    }
+void on_wifi_ready() {
+    gpio_init();
+    homekit_server_init(&config);
 }
 
+void create_accessory_name() {
+    uint8_t macaddr[6];
+    sdk_wifi_get_macaddr(STATION_IF, macaddr);
+
+    int name_len = snprintf(NULL, 0, "Motion Sensor %02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
+    char *name_value = malloc(name_len+1);
+    snprintf(name_value, name_len+1, "Motion Sensor %02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
+
+    name.value = HOMEKIT_STRING(name_value);
+}
 
 void user_init(void) {
     uart_set_baud(0, 115200);
-
-    wifi_config_init2("Motion Sensor Setup", NULL, on_wifi_config_event);
-    led_init();
-
-    if (toggle_create(sensor_pin, sensor_callback, NULL)) {
-        identify_accessory();
-    }
+    gpio_init();
+    create_accessory_name();
+    wifi_config_init2("Motion Sensor Setup", NULL, on_wifi_ready);
 }
