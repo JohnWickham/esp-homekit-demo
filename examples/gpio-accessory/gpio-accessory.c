80234d2170c0c22ffad4d62c6a514fbc1a43f62f
JohnWickham <john@wjwickham.com>
2020-06-04 12:32:32 -0400

    Uses external characteristic

--- examples/gpio-accessory/gpio-accessory.c
+++ examples/gpio-accessory/gpio-accessory.c
@@ -14,49 +14,53 @@ const int accessory_gpio = 12;
 const int onboard_led_gpio = 2;
 bool accessory_on = false;
 
-void write(int pin, bool on) {
-    gpio_write(pin, on ? 0 : 1);
-}
-
 void accessory_init() {
     gpio_enable(onboard_led_gpio, GPIO_OUTPUT);
     gpio_enable(accessory_gpio, GPIO_OUTPUT);   
 }
 
-void led_identify_task(void *_args) {
+void accessory_identify_task(void *_args) {
     for (int i=0; i<3; i++) {
         for (int j=0; j<2; j++) {
-            write(onboard_led_gpio, true);
+            gpio_write(onboard_led_gpio, true);
             vTaskDelay(100 / portTICK_PERIOD_MS);
-            write(onboard_led_gpio, false);
+            gpio_write(onboard_led_gpio, false);
             vTaskDelay(100 / portTICK_PERIOD_MS);
         }
 
         vTaskDelay(250 / portTICK_PERIOD_MS);
     }
 
-    write(onboard_led_gpio, accessory_on);
+    gpio_write(onboard_led_gpio, accessory_on);
 
     vTaskDelete(NULL);
 }
 
-void led_identify(homekit_value_t _value) {
-    xTaskCreate(led_identify_task, "LED identify", 128, NULL, 2, NULL);
+void accessory_identify(homekit_value_t _value) {
+    xTaskCreate(accessory_identify_task, "LED identify", 128, NULL, 2, NULL);
 }
 
-homekit_value_t accessory_on_get() {
-    return HOMEKIT_BOOL(accessory_on);
+homekit_characteristic_t accessory_on = HOMEKIT_CHARACTERISTIC_(
+    ON, false, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(switch_on_callback)
+);
+
+void accessory_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context) {
+    gpio_write(accessory_on.value.bool_value);
 }
 
-void accessory_on_set(homekit_value_t value) {
-    if (value.format != homekit_format_bool) {
-        printf("Invalid value format: %d\n", value.format);
-        return;
-    }
+// homekit_value_t accessory_on_get() {
+//     return HOMEKIT_BOOL(accessory_on);
+// }
 
-    accessory_on = value.bool_value;
-    write(accessory_gpio, accessory_on);
-}
+// void accessory_on_set(homekit_value_t value) {
+//     if (value.format != homekit_format_bool) {
+//         printf("Invalid value format: %d\n", value.format);
+//         return;
+//     }
+
+//     accessory_on = value.bool_value;
+//     write(accessory_gpio, accessory_on);
+// }
 
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "Switch");
 
@@ -68,16 +72,12 @@ homekit_accessory_t *accessories[] = {
             HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "20200601"),
             HOMEKIT_CHARACTERISTIC(MODEL, "Triumph 1–P"),
             HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
-            HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, accessory_identify),
             NULL
         }),
         HOMEKIT_SERVICE(SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]){
             HOMEKIT_CHARACTERISTIC(NAME, "Accent Light"),
-            HOMEKIT_CHARACTERISTIC(
-                ON, false,
-                .getter=accessory_on_get,
-                .setter=accessory_on_set
-            ),
+            &accessory_on,
             NULL
         }),
         NULL
@@ -103,13 +103,12 @@ void create_accessory_name() {
 }
 
 void on_wifi_ready() {
-    create_accessory_name();
     homekit_server_init(&config);
 }
 
 void user_init(void) {
     uart_set_baud(0, 115200);
     accessory_init();
-
+    create_accessory_name();
     wifi_config_init("Accessory Setup", NULL, on_wifi_ready);
 }
