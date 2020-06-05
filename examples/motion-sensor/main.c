a090da7adcc6a89f5621d534eb925fa4a2a6ab74
JohnWickham <john@wjwickham.com>
2020-06-05 10:48:42 -0400

    turn off led when init finishes

--- examples/motion-sensor/main.c
+++ examples/motion-sensor/main.c
@@ -17,7 +17,6 @@ const int led_gpio = 2;
 const int sensor_gpio = 14;
 
 void identify_task(void *_args) {
-    // We identify the `Motion Sensor` by Flashing it's LED.
     for (int i=0; i<3; i++) {
         for (int j=0; j<2; j++) {
             gpio_write(led_gpio, true);
@@ -40,13 +39,6 @@ void identify(homekit_value_t _value) {
 
 homekit_characteristic_t motion_detected  = HOMEKIT_CHARACTERISTIC_(MOTION_DETECTED, 0);
 
-// void motion_sensor_callback(uint8_t gpio) {
-//     int new = gpio_read(sensor_gpio);
-//     motion_detected.value = HOMEKIT_BOOL(new);
-//     homekit_characteristic_notify(&motion_detected, HOMEKIT_BOOL(new));
-//     gpio_write(led_gpio, new);
-// }
-
 void sensor_callback(bool high, void *context) {
     motion_detected.value = HOMEKIT_UINT8(high ? 1 : 0);
     homekit_characteristic_notify(&motion_detected, motion_detected.value);
@@ -54,37 +46,33 @@ void sensor_callback(bool high, void *context) {
 
 void gpio_init() {
     gpio_enable(led_gpio, GPIO_OUTPUT);
-    gpio_write(led_gpio, false);
 
-    // gpio_enable(sensor_gpio, GPIO_INPUT);
-    // gpio_set_pullup(sensor_gpio, false, false);
-    // gpio_set_interrupt(sensor_gpio, GPIO_INTTYPE_EDGE_ANY, motion_sensor_callback);
     if (toggle_create(sensor_gpio, sensor_callback, NULL)) {
-        identify_task(NULL);
+        gpio_write(led_gpio, false);
     }
 }
 
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "Motion Sensor");
 
 homekit_accessory_t *accessories[] = {
-        HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]){
-                HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
-                        &name,
-                        HOMEKIT_CHARACTERISTIC(MANUFACTURER, "John Wickham"),
-                        HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "20200603"),
-                        HOMEKIT_CHARACTERISTIC(MODEL, "Motion Sensor"),
-                        HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
-                        HOMEKIT_CHARACTERISTIC(IDENTIFY, identify),
-                        NULL
-                }),
-                HOMEKIT_SERVICE(MOTION_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]){
-                        HOMEKIT_CHARACTERISTIC(NAME, "Motion Sensor"),
-                        &motion_detected,
-                        NULL
-                }),
-                NULL
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]){
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+            &name,
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "John Wickham"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "20200603"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "Motion Sensor"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(MOTION_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Motion Sensor"),
+            &motion_detected,
+            NULL
         }),
         NULL
+    }),
+    NULL
 };
 
 homekit_server_config_t config = {
